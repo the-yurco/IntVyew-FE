8451b410d91226010341f31a7af79b0cc61da047
the-yurco <jurajbosy1@gmail.com>
2024-05-13 20:31:01 +0200

    feat: added 2 more pages from header

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ArticlePage from './pages/ArticlePage';
+import AboutPage from './pages/AboutPage';
+import ContactPage from './pages/ContactPage';
 
 const App: React.FC = () => {
 	return (
@@ -9,6 +11,8 @@ const App: React.FC = () => {
 			<Routes>
 				<Route path="/" element={<HomePage />} />
 				<Route path="/article/:url" element={<ArticlePage />} />
+				<Route path="/about" element={<AboutPage />} />
+				<Route path="/contact" element={<ContactPage />} />
 			</Routes>
 		</BrowserRouter>
 	);
