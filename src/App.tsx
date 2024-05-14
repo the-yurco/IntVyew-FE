ddbacc55bc1308f5f680856fff872bcdee8dc072
the-yurco <jurajbosy1@gmail.com>
2024-05-14 14:15:02 +0200

    feat: almost done

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
-import ArticlePage from './pages/ArticlePage';
 import AboutPage from './pages/AboutPage';
 import ContactPage from './pages/ContactPage';
 
@@ -10,7 +9,6 @@ const App: React.FC = () => {
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" element={<HomePage />} />
-				{/* <Route path="/article/:url" element={<ArticlePage />} /> */}
 				<Route path="/about" element={<AboutPage />} />
 				<Route path="/contact" element={<ContactPage />} />
 			</Routes>
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,17 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import ArticleListItem from '../components/ArticleListItem';
+import Search from '../components/Search';
 import { getTopHeadlines } from '../services/ApiServices';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 
 const HomePage: React.FC = () => {
 	const [articles, setArticles] = useState<any[]>([]);
+	const [filteredArticles, setFilteredArticles] = useState<any[]>([]);
 
 	useEffect(() => {
 		const fetchArticles = async () => {
 			try {
 				const data = await getTopHeadlines();
 				setArticles(data.articles);
+				setFilteredArticles(data.articles); // Initialize filteredArticles with all articles
 			} catch (error) {
 				console.error('Error fetching articles:', error);
 			}
@@ -20,15 +23,23 @@ const HomePage: React.FC = () => {
 		fetchArticles();
 	}, []);
 
+	const handleSearch = (query: string) => {
+		const filtered = articles.filter((article) =>
+			article.title.toLowerCase().includes(query.toLowerCase())
+		);
+		setFilteredArticles(filtered);
+	};
+
 	return (
 		<>
 			<Header />
 			<div className="home-container">
 				<div className="home-head">
 					<h1 className="home-title">Latest News</h1>
+					<Search onSearch={handleSearch} />
 				</div>
 				<div className="home-grid">
-					{articles.map((article: any, index: number) => (
+					{filteredArticles.map((article: any, index: number) => (
 						<ArticleListItem key={index} article={article} />
 					))}
 				</div>
